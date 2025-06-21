55b6996dccb4ce20adcc34dd0be034f761483e93
JB <198005711+baumjessi@users.noreply.github.com>
2025-06-21 12:43:18 -0500

    Create function to display a card for each book
    Create function that loops through all objects in the "myLibrary" array
    and displays them as cards on the page. Right now, the cards are filled
    with dummy content. Styled h1 and p elements containing titles, authors,
    etc. will be added next.

--- main.js
+++ main.js
@@ -6,19 +6,35 @@ function Book(title, author, pages, hasRead) {
   this.author = author;
   this.pages = pages;
   this.hasRead = hasRead;
+  this.UUID = crypto.randomUUID();
+  myLibrary.push(this);
   console.log(this.title);
   console.log(this.author);
   console.log(this.pages);
   console.log(this.hasRead);
+  console.log(this.UUID);
 }
-
-const book1 = Book("Dune", "Frank Herbert", "200", "true");
-const book2 = Book("Cruddy", "Lynda Barry", "200", "false");
 
 function addBookToLibrary(title, author, pages, hasRead) {
-    const newBook = Book(title, author, pages, hasRead);
-    myLibrary.push(newBook);
+    const newBook = new Book(title, author, pages, hasRead);
     console.log(newBook);
-    console.log(myLibrary);
 }
 
+addBookToLibrary("Great Value", "Jessi B.", 170, true);
+addBookToLibrary("Shrike House", "Jessi B.", 200, false);
+addBookToLibrary("Dune", "Frank Herbert", 700, false);
+
+console.table(myLibrary);
+
+let cardBody = document.getElementById("card-body");
+
+function displayBook() {
+  myLibrary.forEach(Book => {
+    let newCard = document.createElement('div');
+    newCard.textContent = `${Book.title} by ${Book.author} is ${Book.pages} long.`;
+    newCard.className = "card";
+    cardBody.appendChild(newCard);
+  }
+)};
+
+displayBook(myLibrary);
