214fecc45eadd4ae7b4979deffcc2852703b1202
JB <198005711+baumjessi@users.noreply.github.com>
2025-06-22 11:02:36 -0500

    Add "new book" dialog box & update nav stylings
    Add a dialog box that appears when the user clicks the "add new book"
    button. Flesh out the skeleton of a dialog form that collects the
    various properties of the book. Style the top navigation to enhance
     user experience.

--- main.js
+++ main.js
@@ -31,10 +31,52 @@ let cardBody = document.getElementById("card-body");
 function displayBook() {
   myLibrary.forEach(Book => {
     let newCard = document.createElement('div');
-    newCard.textContent = `${Book.title} by ${Book.author} is ${Book.pages} long.`;
     newCard.className = "card";
     cardBody.appendChild(newCard);
-  }
+    let title = document.createElement('h1');
+    title.textContent = `${Book.title}`;
+    newCard.appendChild(title);
+    let author = document.createElement('p');
+    author.textContent = `Author: ${Book.author}`;
+    newCard.appendChild(author);
+    let pages = document.createElement('p');
+    pages.textContent = `Pages: ${Book.pages}`;
+    newCard.appendChild(pages);
+    let hasRead = document.createElement('p');
+    newCard.appendChild(hasRead);
+    if (hasRead = true) {
+      hasRead.textContent = "Read! :)";
+    }
+    else if (hasRead = false) {
+      hasRead.textContent = "Not read yet. :(";
+    }
+    }
 )};
 
 displayBook(myLibrary);
+
+const addBtn = document.getElementById("add-btn");
+const newBookDialog = document.getElementById("new-book-dialog");
+newBookDialog.returnValue = "new Book";
+
+addBtn.addEventListener("click", (e)=> {
+  newBookDialog.showModal();
+});
+
+const dialogCloseBtn = document.getElementById("dialog-close");
+
+dialogCloseBtn.addEventListener("click", e => {
+  newBookDialog.close();
+});
+
+newBookDialog.addEventListener("click", e => {
+  const dialogDimensions = newBookDialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+   newBookDialog.close()
+  }
+})
