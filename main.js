b14324b9011a4cb673d0b323d897b08efc397a0f
JB <198005711+baumjessi@users.noreply.github.com>
2025-06-26 11:08:34 -0500

    Make remove button functional for new adds
    The remove button for any cards created using the "new book" dialog is
    now functional. The remove button for cards generated on page load is
    not yet working.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myLibrary = [
+let myLibrary = [
 ];
 
 function Book(title, author, pages, hasRead) {
@@ -59,13 +59,61 @@ function displayAllBooks() {
 
 displayAllBooks(myLibrary);
 
-let removeBtn = document.querySelectorAll("button.remove-btn");
-let removeBtnArray = Array.from(removeBtn);
+let newBookForm = document.getElementById("new-book-form");
+let submitBookBtn = document.getElementById("dialog-submit");
+  submitBookBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let pages = document.getElementById('pages');
+    let hasRead = true;
+    let newBook = {
+      title: title.value,
+      author: author.value,
+      pages: pages.value,
+      hasRead: hasRead,
+      UUID: crypto.randomUUID(),
+    }
+    myLibrary.push(newBook);
 
-removeBtnArray.forEach(button => {
-  button.addEventListener("click", e => {
-  window.alert("Poopee Peepee");
-  })});
+    let newCard = document.createElement('div');
+    newCard.className = "card";
+    newCard.setAttribute("data-UUID", newBook.UUID);
+    cardBody.appendChild(newCard);
+    let newTitle = document.createElement('h1');
+    newTitle.textContent = `${title.value}`;
+    newCard.appendChild(newTitle);
+    let newAuthor = document.createElement('p');
+    newAuthor.textContent = `Author: ${author.value}`;
+    newCard.appendChild(newAuthor);
+    let newPages = document.createElement('p');
+    newPages.textContent = `Pages: ${pages.value}`;
+    newCard.appendChild(newPages);
+    let newHasRead = document.createElement('p');
+    newCard.appendChild(newHasRead);
+    if (newHasRead = true) {
+      newHasRead.textContent = "Read! :)";
+    }
+    else if (newHasRead = false) {
+      newHasRead.textContent = "Not read yet. :(";
+    }
+    let removeBtn = document.createElement('button');
+    removeBtn.textContent = "Remove";
+    removeBtn.className = "remove-btn";
+    removeBtn.setAttribute("data-UUID", newBook.UUID);
+    newCard.appendChild(removeBtn);
+    removeBtn.addEventListener("click", e => {
+      if (removeBtn.getAttribute("data-UUID") === newCard.getAttribute("data-UUID")) {
+      newCard.remove();
+      const newLibrary = myLibrary.filter(book => book.UUID !== removeBtn.getAttribute("data-UUID"));
+      myLibrary = newLibrary;
+      console.log(myLibrary);
+      }
+    });
+
+    console.table(myLibrary);
+    newBookDialog.close();
+});
 
 const addBtn = document.getElementById("add-btn");
 const newBookDialog = document.getElementById("new-book-dialog");
@@ -93,47 +141,18 @@ newBookDialog.addEventListener("click", e => {
   }
 })
 
-let newBookForm = document.getElementById("new-book-form");
-let submitBookBtn = document.getElementById("dialog-submit");
-  submitBookBtn.addEventListener("click", e => {
-    e.preventDefault();
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let pages = document.getElementById('pages');
-    let hasRead = true;
-    let newBook = {
-      title: title.value,
-      author: author.value,
-      pages: pages.value,
-      hasRead: hasRead,
-      UUID: crypto.randomUUID(),
-    }
-    myLibrary.push(newBook);
+let removeBtn = document.querySelectorAll("button.remove-btn");
+let removeBtnArray = Array.from(removeBtn);
+let card = document.querySelectorAll(".card");
+let cardArray = Array.from(card);
 
-    let newCard = document.createElement('div');
-    newCard.className = "card";
-    cardBody.appendChild(newCard);
-    let newTitle = document.createElement('h1');
-    newTitle.textContent = `${title.value}`;
-    newCard.appendChild(newTitle);
-    let newAuthor = document.createElement('p');
-    newAuthor.textContent = `Author: ${author.value}`;
-    newCard.appendChild(newAuthor);
-    let newPages = document.createElement('p');
-    newPages.textContent = `Pages: ${pages.value}`;
-    newCard.appendChild(newPages);
-    let newHasRead = document.createElement('p');
-    newCard.appendChild(newHasRead);
-    if (newHasRead = true) {
-      newHasRead.textContent = "Read! :)";
+removeBtnArray.forEach(button => {
+  button.addEventListener("click", e => {
+    cardArray.forEach(card => {
+    if (this.getAttribute("data-UUID") === card.getAttribute("data-UUID")) {
+      card.remove();
+      const newLibrary = myLibrary.filter(book => book.UUID !== button.getAttribute("data-UUID"));
+      myLibrary = newLibrary;
+      console.log(myLibrary);
     }
-    else if (newHasRead = false) {
-      newHasRead.textContent = "Not read yet. :(";
-    }
-    let removeBtn = document.createElement('button');
-    removeBtn.textContent = "Remove";
-    removeBtn.className = "remove-btn";
-    newCard.appendChild(removeBtn);
-    console.table(myLibrary);
-    newBookDialog.close();
-});
+    })})});
